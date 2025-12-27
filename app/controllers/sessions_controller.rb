35406583c5e15f472106249dfe410a54af674b01
あなたの名前 <>
2025-12-28 03:55:27 +0900

    add: cloudinary設定

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,5 +8,24 @@ class SessionsController < ApplicationController
   session[:is_guest] = true 
   
   redirect_to recipes_path, notice: "ゲストとしてログインしました（閲覧専用）"
-end
+  end
+
+  def google_auth_callback
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_by!(provider: auth.provider, uid: auth.uid) do |u|
+      u.name = auth.info.name
+      u.email = auth.info.email
+      u.image_url = auth.info.image # Googleの画像URLを一旦保存
+    end
+
+    # Googleの画像をCloudinaryに転送して保存する処理
+    if user.image_url.present? && !user.avatar.attached?
+      require 'open-uri'
+      file = URI.open(user.image_url)
+      user.avatar.attach(io: file, filename: "user_#{user.uid}.jpg", content_type: 'image/jpg')
+    end
+
+    session[:user_id] = user.id
+    redirect_to recipes_path, notice: "ログインしました！"
+  end
 end
