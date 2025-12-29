8174fa968bb05a94345bf052db8944ced2d6c67c
あなたの名前 <>
2025-12-30 01:43:05 +0900

    fix: rubocop修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,6 @@ class ApplicationController < ActionController::Base
 
 
   def guest_user?
-    current_user&.email == 'guest@example.com'
+    current_user&.email == "guest@example.com"
   end
-
 end
