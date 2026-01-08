6b547f8205a6e1d2827e51ada60344f998ef7e9a
あなたの名前 <>
2026-01-09 02:54:06 +0900

    公開/非公開の表示分け実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,17 @@
 class RecipesController < ApplicationController
   def index
-  @recipes = Recipe.with_attached_image.includes(:convenience_food).order(created_at: :desc)
+    base_query = Recipe.with_attached_image.includes(:convenience_food)
+
+    if params[:filter] == "mine" && current_user
+      @recipes = base_query.where(user_id: current_user.id)
+                          .order(created_at: :desc)
+      @current_filter = "mine"
+    else
+      # デフォルト：公開レシピのみ（みんなのレシピ）
+      @recipes = base_query.where(is_public: true)
+                          .order(created_at: :desc)
+      @current_filter = "public"
+    end
   end
 
   def new
@@ -9,20 +20,42 @@ class RecipesController < ApplicationController
     5.times { @recipe.recipe_ingredients.build }
   end
 
-def create
-  @recipe = Recipe.new(recipe_params)
-  @recipe.user_id = current_user.id
-
-  if params[:recipe][:steps].is_a?(Array)
-    @recipe.steps = params[:recipe][:steps].select(&:present?).join("\n")
+  def show
+  @recipe = Recipe.find(params[:id])
+  
+    if !@recipe.is_public? && @recipe.user != current_user
+      redirect_to recipes_path, alert: "このレシピは非公開です"
+    end
   end
 
-  if @recipe.save
-    redirect_to @recipe, notice: "レシピを投稿しました！"
-  else
-    render :new, status: :unprocessable_entity
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.user_id = current_user.id
+
+    if params[:recipe][:steps].is_a?(Array)
+      @recipe.steps = params[:recipe][:steps].select(&:present?).join("\n")
+    end
+
+    if @recipe.save
+      redirect_to recipes_path, notice: "レシピを投稿しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    # 自分のレシピ以外は操作できないようにガード
+    return redirect_to recipes_path unless @recipe.user == current_user
+
+    @recipe.update(is_public: !@recipe.is_public)
+    
+    # Turboを使って一部分だけ書き換える（または一覧へ戻る）
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to recipes_path }
+    end
+  end
 
 private
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
   accepts_nested_attributes_for :recipe_ingredients, allow_destroy: true, reject_if: :all_blank
+  after_initialize :set_default_is_public, if: :new_record?
 
   def total_cost
     recipe_ingredients.to_a.sum(&:total_price)
@@ -12,5 +13,11 @@ class Recipe < ApplicationRecord
 
   def savings_amount
     convenience_food.price - total_cost
+  end
+
+  private
+
+  def set_default_is_public
+    self.is_public ||= false
   end
 end
