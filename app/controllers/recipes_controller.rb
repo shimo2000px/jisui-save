0fc2234d21e8369021adcc0f3a56b62c520062b9
あなたの名前 <>
2026-01-09 02:24:15 +0900

    一覧画面レイアウト調整

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = [
-      # これは仮のものです
-      { title: "ふんわり卵の節約親子丼", cost: 150, time: 15, image: "🥚" },
-      { title: "豆苗と豚肉のシャキシャキ炒め", cost: 200, time: 10, image: "🌱" },
-      { title: "大根たっぷり染み旨煮", cost: 100, time: 20, image: "🍲" }
-    ]
+  @recipes = Recipe.with_attached_image.includes(:convenience_food).order(created_at: :desc)
   end
 
   def new
