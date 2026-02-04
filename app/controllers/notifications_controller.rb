db2982c60f0b5d169f96454b41d5379caa98e763
あなたの名前 <>
2026-02-04 13:39:33 +0900

    fix: 新規IDを作成して通知できるように保存

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,19 +6,18 @@ class NotificationsController < ApplicationController
   end
 
   def update
-      logger.debug "--- DEBUG: Params received: #{params[:notification_setting].inspect} ---"
-      @setting = current_user.notification_setting
+    @setting = current_user.notification_setting || current_user.build_notification_setting
 
-      p_hash = notification_params
-      if p_hash[:send_time].present?
-        p_hash[:send_time] = Time.zone.parse(p_hash[:send_time])
-      end
+    p_hash = notification_params
+    if p_hash[:send_time].present?
+      p_hash[:send_time] = Time.zone.parse(p_hash[:send_time])
+    end
 
-      if @setting.update(p_hash)
-        redirect_to profile_path, notice: "通知設定を更新しました"
-      else
-        render :edit, status: :unprocessable_entity
-      end
+    if @setting.update(p_hash)
+      redirect_to profile_path, notice: "通知設定を保存しました（ID:#{@setting.id}）"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
