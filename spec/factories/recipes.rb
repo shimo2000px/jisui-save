958b2ff795fe308f3133fe7c12770122957847b6
あなたの名前 <>
2026-02-12 16:58:58 +0900

    fix: CLテストエラー修正

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -4,8 +4,10 @@ FactoryBot.define do
     association :user
     association :convenience_food
 
-      after(:build) do |recipe|
-        recipe.recipe_ingredients << build(:recipe_ingredient, recipe: recipe)
+    after(:build) do |recipe|
+      if recipe.recipe_ingredients.empty?
+        recipe.recipe_ingredients << build(:recipe_ingredient, recipe: recipe) 
+      end
     end
   end
 end
